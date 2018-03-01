722bb9bcee13f2922b851ade4ce456a10aa99d58
Ashley Margallo <amarie921@yahoo.com>
2018-03-01 15:42:59 -0700

    initial song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301220515) do
+ActiveRecord::Schema.define(version: 20180301222207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,13 @@ ActiveRecord::Schema.define(version: 20180301220515) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  add_foreign_key "songs", "artists"
 end

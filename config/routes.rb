b93b5f493af5663ee4d9868cd98be200a6605a11
Becca Tew <rebeccatew@gmail.com>
2018-03-01 15:25:17 -0700

    artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   resources :magazines
 
   root 'magazines#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
  

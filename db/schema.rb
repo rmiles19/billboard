74f5d7217a61fa195c774aff2cb8f069051a625b
Becca Tew <rebeccatew@gmail.com>
2018-03-01 17:12:10 -0700

    artist new

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301214511) do
+ActiveRecord::Schema.define(version: 20180301220515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "magazines", force: :cascade do |t|
     t.string "list_name"
